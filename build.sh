93ab7b1ab92308236dc45ade0556ebbb475f6dc3
Atal Kumar <atalkumar.me@gmail.com>
2025-10-10 18:14:00 +0530

    fix media folder path

--- build.sh
+++ build.sh
@@ -6,4 +6,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Import vans data after migrations
-python manage.py import_vans || echo "Skipping import_vans if already imported"
+# python manage.py import_vans || echo "Skipping import_vans if already imported"
+
+echo "📂 Checking media folder contents:"
+ls -R media/
--- config/settings.py
+++ config/settings.py
@@ -176,4 +176,4 @@ REST_FRAMEWORK = {
 
 # Media files (for van images)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
