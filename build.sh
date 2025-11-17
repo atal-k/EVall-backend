9919eb1f063b1caffe74167cdee0fc99d4fca1aa
Atal Kumar <atalkumar.me@gmail.com>
2025-11-17 13:25:54 +0530

    update management cmd

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Import vans data after migrations
-# python manage.py import_vans || echo "Skipping import_vans if already imported"
+python manage.py create_superuser_if_none
